be90cd8032262c9b9b4a423506f812ce6873471c
ujjwal6bitquasar <ujjwal264dwivedi@gmail.com>
2026-02-26 18:24:19 +0530

    deploying frontend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ import doctorRoutes from './routes/doctorRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
