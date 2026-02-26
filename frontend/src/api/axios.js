c882ed23b2bb650aceb085bb206bf46ad44f995a
ujjwal6bitquasar <ujjwal264dwivedi@gmail.com>
2026-02-26 18:03:30 +0530

    frontend push

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
-// Automatically attach token to every request if it exists
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
